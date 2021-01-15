781406516f219359a4b26f978df43d062e850a56
ParkJuhyeong <majecty@gmail.com>
2021-01-15 13:17:22 +0900

    Add console print helper

--- src/cljs/multi_client_ws/core.cljs
+++ src/cljs/multi_client_ws/core.cljs
@@ -1,5 +1,11 @@
 (ns multi-client-ws.core)
 
+(defn js-console-f [text & args]
+  (.log js/console text ": " args))
+
+(defn js-console [& args]
+  (.log js/console args))
+
 (defn mount-components []
   (let [content (js/document.getElementById "app")]
     (while (.hasChildNodes content)
