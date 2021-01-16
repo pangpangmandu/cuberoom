025843de727ec2de8ac627df0e8c359c96e5ad65
ParkJuhyeong <majecty@gmail.com>
2021-01-16 12:26:27 +0900

    Format project.clj

--- project.clj
+++ project.clj
@@ -31,7 +31,7 @@
                  [selmer "1.12.31"]]
 
   :min-lein-version "2.0.0"
-  
+
   :source-paths ["src/clj" "src/cljs" "src/cljc"]
   :test-paths ["test/clj"]
   :resource-paths ["resources" "target/cljsbuild"]
@@ -49,33 +49,32 @@
    :nrepl-port 7002
    :css-dirs ["resources/public/css"]
    :nrepl-middleware [cider.piggieback/wrap-cljs-repl]}
-  
 
   :profiles
   {:uberjar {:omit-source true
              :prep-tasks ["compile" ["cljsbuild" "once" "min"]]
-             :cljsbuild{:builds
-              {:min
-               {:source-paths ["src/cljc" "src/cljs" "env/prod/cljs"]
-                :compiler
-                {:output-dir "target/cljsbuild/public/js"
-                 :output-to "target/cljsbuild/public/js/app.js"
-                 :source-map "target/cljsbuild/public/js/app.js.map"
-                 :optimizations :advanced
-                 :pretty-print false
-                 :infer-externs true
-                 :closure-warnings
-                 {:externs-validation :off :non-standard-jsdoc :off}}}}}
-             
+             :cljsbuild {:builds
+                         {:min
+                          {:source-paths ["src/cljc" "src/cljs" "env/prod/cljs"]
+                           :compiler
+                           {:output-dir "target/cljsbuild/public/js"
+                            :output-to "target/cljsbuild/public/js/app.js"
+                            :source-map "target/cljsbuild/public/js/app.js.map"
+                            :optimizations :advanced
+                            :pretty-print false
+                            :infer-externs true
+                            :closure-warnings
+                            {:externs-validation :off :non-standard-jsdoc :off}}}}}
+
              :aot :all
              :uberjar-name "multi-client-ws.jar"
-             :source-paths ["env/prod/clj" ]
+             :source-paths ["env/prod/clj"]
              :resource-paths ["env/prod/resources"]}
 
    :dev           [:project/dev :profiles/dev]
    :test          [:project/dev :project/test :profiles/test]
 
-   :project/dev  {:jvm-opts ["-Dconf=dev-config.edn" ]
+   :project/dev  {:jvm-opts ["-Dconf=dev-config.edn"]
                   :dependencies [[binaryage/devtools "1.0.2"]
                                  [cider/piggieback "0.5.2"]
                                  [doo "0.1.11"]
@@ -87,31 +86,30 @@
                   :plugins      [[com.jakemccrary/lein-test-refresh "0.24.1"]
                                  [jonase/eastwood "0.3.5"]
                                  [lein-doo "0.1.11"]
-                                 [lein-figwheel "0.5.20"]] 
-                  :cljsbuild{:builds
-                   {:app
-                    {:source-paths ["src/cljs" "src/cljc" "env/dev/cljs"]
-                     :figwheel {:on-jsload "multi-client-ws.core/mount-components"}
-                     :compiler
-                     {:main "multi-client-ws.app"
-                      :asset-path "/js/out"
-                      :output-to "target/cljsbuild/public/js/app.js"
-                      :output-dir "target/cljsbuild/public/js/out"
-                      :source-map true
-                      :optimizations :none
-                      :pretty-print true}}}}
-                  
-                  
+                                 [lein-figwheel "0.5.20"]]
+                  :cljsbuild {:builds
+                              {:app
+                               {:source-paths ["src/cljs" "src/cljc" "env/dev/cljs"]
+                                :figwheel {:on-jsload "multi-client-ws.core/mount-components"}
+                                :compiler
+                                {:main "multi-client-ws.app"
+                                 :asset-path "/js/out"
+                                 :output-to "target/cljsbuild/public/js/app.js"
+                                 :output-dir "target/cljsbuild/public/js/out"
+                                 :source-map true
+                                 :optimizations :none
+                                 :pretty-print true}}}}
+
                   :doo {:build "test"}
-                  :source-paths ["env/dev/clj" ]
+                  :source-paths ["env/dev/clj"]
                   :resource-paths ["env/dev/resources"]
                   :repl-options {:init-ns user
                                  :timeout 120000}
                   :injections [(require 'pjstadig.humane-test-output)
                                (pjstadig.humane-test-output/activate!)]}
-   :project/test {:jvm-opts ["-Dconf=test-config.edn" ]
-                  :resource-paths ["env/test/resources"] 
-                  :cljsbuild 
+   :project/test {:jvm-opts ["-Dconf=test-config.edn"]
+                  :resource-paths ["env/test/resources"]
+                  :cljsbuild
                   {:builds
                    {:test
                     {:source-paths ["src/cljc" "src/cljs" "test/cljs"]
@@ -119,8 +117,7 @@
                      {:output-to "target/test.js"
                       :main "multi-client-ws.doo-runner"
                       :optimizations :whitespace
-                      :pretty-print true}}}}
-                  
-                  }
+                      :pretty-print true}}}}}
+
    :profiles/dev {}
    :profiles/test {}})
