921de89c0d8698590e72ebd6a161222387ad4aa6
ParkJuhyung <majecty@gmail.com>
2021-04-11 20:17:02 +0900

    Set playerStartPos to nearImages
    It helps debugging player interaction.

--- try2-js/src/entity/player/const.js
+++ try2-js/src/entity/player/const.js
@@ -1,6 +1,17 @@
 /* eslint-disable import/prefer-default-export */
 
-export const playerStartPos = {
+/* eslint-disable no-unused-vars */
+const hallUpperImages = {
   x: 800,
   y: 600,
 };
+
+/**
+ * 이미지 인터렉션 디버깅용
+ */
+const nearImages = {
+  x: 16 * 53 + 1,
+  y: 16 * 53 + 10,
+};
+
+export const playerStartPos = nearImages;
