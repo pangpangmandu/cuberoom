11d425c8f21679e9b1265f3905f82e242f366a34
leegakyeong <alaska7132@naver.com>
2021-07-15 21:51:16 +0900

    Set routes

--- src/App.svelte
+++ src/App.svelte
@@ -1,138 +1,30 @@
 <script>
-	// export let name;
-
-import Phaser from "phaser";
-import { log } from "./log";
-import { playerCreate, playerUpdate } from "./entity/player";
-import { allCharacterImageNames } from "./entity/player/image";
-import { playerCreateAnimations } from "./entity/player/animation";
-import { mapCreate, mapCreateOverCharacterLayer } from "./entity/map";
-import {
-	mapOnPointerDown,
-	mapUpdateMousePoint,
-} from "./entity/map/interaction";
-import { playerOnMapCreate, playerOnMapUpdate } from "./relation/playerOnMap";
-
-function backgroundStatic(scene) {
-	scene.add.sprite(1920 / 2, 1088 / 2, "background");
-}
-
-class CuberoomScene extends Phaser.Scene {
-	constructor() {
-		super();
-		this.map = null;
-		this.player = null;
-		this.cursors = null;
-		this.playerOnMap = null;
-		this.popups = [];
-	}
-
-	preload() {
-		this.load.image("background", "/img/art-0416.png");
-		this.load.image("collision-tileset", "/tilemap/simple_tile.png");
-		this.load.image("interactive-tile", "/tilemap/interactive-tile.png");
-		this.load.image("popup", "/img/popup.png");
-		this.load.tilemapTiledJSON({
-			key: "map",
-			url: "/tilemap/tilemap.json",
-		});
-		for (const [key, file] of allCharacterImageNames()) {
-			this.load.image(key, file);
-			log("player image load ", [key, file]);
-		}
-	}
-
-	create() {
-		playerCreateAnimations(this);
-		backgroundStatic(this);
-
-		this.map = mapCreate(this);
-		this.player = playerCreate(this);
-		this.playerOnMap = playerOnMapCreate();
-		this.physics.add.collider(this.player.phaser, this.map.collisionLayer);
-
-		this.map = mapCreateOverCharacterLayer(this.map);
-
-		this.cameras.main.setBounds(
-			0,
-			0,
-			this.map.phaser.widthInPixels,
-			this.map.phaser.heightInPixels
-		);
-		this.cameras.main.startFollow(this.player.phaser, true, 0.1, 0.1);
-
-		this.cursors = this.input.keyboard.createCursorKeys();
-		this.input.keyboard.on("keydown-SPACE", () => {
-			log("Space");
-			if (this.cheat === true) {
-				this.cheat = false;
-			} else {
-				this.cheat = true;
-			}
-		});
-
-		this.input.on("pointerdown", (pointer) =>
-			mapOnPointerDown(this.map, pointer)
-		);
-	}
-
-	update(_time, _delta) {
-		this.player = playerUpdate(this.player, this.cursors, this);
-		mapUpdateMousePoint(this.map, this);
-		this.playerOnMap = playerOnMapUpdate(
-			this.playerOnMap,
-			this.player,
-			this.map,
-			this
-		);
-	}
-}
-
-const config = {
-	type: Phaser.AUTO,
-	zoom: 2,
-	parent: "phaser-parent",
-	width: window.innerWidth / 2,
-	height: window.innerHeight / 2,
-
-	pixelArt: true,
-	physics: {
-		default: "arcade",
-		arcade: {
-			gravity: { y: 0 },
-		},
-	},
-	scene: [CuberoomScene],
-};
-
-const game = new Phaser.Game(config);
-window.game = game;
-
+	import { Router, Route } from 'svelte-navigator';
+	import IntroPage from './pages/Intro.svelte';
+	import CharacterSelection from './pages/CharacterSelection.svelte';
+	import Map from './pages/Map.svelte';
+	import Entrance from './pages/Entrance.svelte';
+	import FirstFloor from './pages/FirstFloor.svelte';
+	import FirstBasement from './pages/FirstBasement.svelte';
 </script>
 
-<!-- <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main> -->
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+<Router>
+	<Route path="/">
+		<IntroPage />
+	</Route>
+	<Route path="character-selection">
+		<CharacterSelection />
+	</Route>
+	<Route path="map">
+		<Map />
+	</Route>
+	<Route path="entrance">
+		<Entrance />
+	</Route>
+	<Route path="1f">
+		<FirstFloor />
+	</Route>
+	<Route path="b1">
+		<FirstBasement />
+	</Route>
+</Router>
