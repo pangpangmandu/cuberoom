0d2f843d65b3bbee5e161113b75b413244aeb5ae
leegakyeong <alaska7132@naver.com>
2021-07-17 01:09:29 +0900

    Update Map page

--- src/pages/Map.svelte
+++ src/pages/Map.svelte
@@ -1,21 +1,19 @@
-<script>
-  import { Link } from 'svelte-navigator';
-</script>
-
 <main>
-  <img
-    src="/img/03_튜토리얼팝업(참조용).png"
-    alt="
-      안녕하세요? 저는 이 작품을 만든 선우 훈 입니다.
-      이 '큐브 스테이지'는 가상 미술관 관람을 위해 제작된 웹사이트입니다.
-      서울 강남구에 있는 코라아나 미술관의 '스페이스 씨'에서 열리는 전시를 위해 만들었습니다.
-      코리아나 미술관으로 가 볼까요? 강남구를 클릭해 주세요.
-    "
-  />
-  <img src="/img/클릭지시버튼.png" alt="클릭" />
-  <Link to="/entrance">
-    <img src="/img/03_서울행정구 배경_01_강남구 버튼.png" alt="결정" />
-  </Link>
+  <div class="guide">
+    <img
+      src="/img/03_튜토리얼팝업(참조용).png"
+      alt="
+        안녕하세요? 저는 이 작품을 만든 선우 훈 입니다.
+        이 '큐브 스테이지'는 가상 미술관 관람을 위해 제작된 웹사이트입니다.
+        서울 강남구에 있는 코라아나 미술관의 '스페이스 씨'에서 열리는 전시를 위해 만들었습니다.
+        코리아나 미술관으로 가 볼까요? 강남구를 클릭해 주세요.
+      "
+    />
+  </div>
+  <img class="flag" src="/img/클릭지시버튼.png" alt="" />
+  <a href="/entrance">
+    <img src="/img/03_서울행정구 배경_01_강남구 버튼.png" alt="클릭" />
+  </a>
 </main>
 
 <style>
@@ -24,5 +22,25 @@
     background-image: url('/img/03_서울행정구 배경_01.jpg');
     background-repeat: no-repeat;
     background-size: cover;
+  }
+
+  .guide {
+    width: 100%;
+    height: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .flag {
+    position: absolute;
+    top: 400px;
+    left: 740px;
+  }
+
+  a {
+    position: absolute;
+    top: 500px;
+    left: 740px;
   }
 </style>
