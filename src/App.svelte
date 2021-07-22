4872330ec7855d00fb2b33f654b58a0cc2df2c14
leegakyeong <alaska7132@naver.com>
2021-07-22 23:21:32 +0900

    Change game pages to scenes

--- src/App.svelte
+++ src/App.svelte
@@ -3,16 +3,12 @@
 	import Intro from './pages/Intro.svelte';
 	import CharacterSelection from './pages/CharacterSelection.svelte';
 	import Map from './pages/Map.svelte';
-	import Entrance from './pages/Entrance.svelte';
-	import FirstFloor from './pages/FirstFloor.svelte';
-	import FirstBasement from './pages/FirstBasement.svelte';
+	import Game from './pages/Game.svelte';
 </script>
 
 <Router>
 	<Route path="/" component="{Intro}" />
 	<Route path="/character-selection" component="{CharacterSelection}" />
 	<Route path="/map" component="{Map}" />
-	<Route path="/entrance" component="{Entrance}" />
-	<Route path="/1f" component="{FirstFloor}" />
-	<Route path="/b1" component="{FirstBasement}" />
+	<Route path="/game" component="{Game}" />
 </Router>
---
+++ src/pages/Game.svelte
@@ -0,0 +1,26 @@
+<script>
+  import Phaser from 'phaser';
+  import EntranceScene from '../scenes/EntranceScene';
+  import FirstFloorScene from '../scenes/FirstFloorScene';
+  import FirstBasementScene from '../scenes/FirstBasementScene';
+
+  const config = {
+    type: Phaser.AUTO,
+    zoom: 2,
+    parent: "phaser-parent",
+    width: window.innerWidth / 2,
+    height: window.innerHeight / 2,
+
+    pixelArt: true,
+    physics: {
+      default: "arcade",
+      arcade: {
+        gravity: { y: 0 },
+      },
+    },
+    scene: [EntranceScene, FirstFloorScene, FirstBasementScene],
+  };
+
+  const game = new Phaser.Game(config);
+  window.game = game;
+</script>
