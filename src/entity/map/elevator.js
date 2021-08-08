0f0a83348bfe0994255a3f5babb60624108c8abe
leegakyeong <alaska7132@naver.com>
2021-08-08 16:57:33 +0900

    Use el-close button to mstart scenes on elevator

--- src/entity/map/elevator.js
+++ src/entity/map/elevator.js
@@ -1,7 +1,9 @@
 let panelContainer;
+let selectedFloor;
 
-function elButton(x, y, floor) {
+function elFloorButton(x, y, floor) {
   const button = document.createElement('button');
+  button.classList.add('floor-button')
   button.style.width = '48px';
   button.style.height = '48px';
   button.style.position = 'absolute';
@@ -9,6 +11,14 @@ function elButton(x, y, floor) {
   button.style.left = `${x}px`;
   button.style.border = 'none';
   button.style.backgroundImage = `url("/img/ui-map/el_${floor}.png")`;
+  button.style.opacity = 0;
+  button.onclick = () => {
+    for (const b of panelContainer.getElementsByClassName('floor-button')) {
+      b.style.opacity = 0;
+    };
+    selectedFloor = floor;
+    button.style.opacity = 1;
+  }
 
   return button;
 }
@@ -35,54 +45,56 @@ export function showElevatorPanel(scene, floor) {
 
   panelContainer.appendChild(panel);
 
-  const buttonTo1F = elButton(56, 276, '1F');
-  buttonTo1F.onclick = () => {
-    hideElevatorPanel();
-    scene.scene.start('FirstFloorScene', { x: 16 * 9, y: 16 * 21 });
-  }
-  panel.appendChild(buttonTo1F);
+  panel.appendChild(elFloorButton(56, 276, '1F'));
+  panel.appendChild(elFloorButton(120, 276, '2F'));
+  panel.appendChild(elFloorButton(56, 212, '5F'));
+  panel.appendChild(elFloorButton(120, 212, '6F'));
+  panel.appendChild(elFloorButton(56, 148, '7F'));
+  panel.appendChild(elFloorButton(120, 340, 'B1'));
+  panel.appendChild(elFloorButton(56, 340, 'B2'));
 
-  const buttonTo2F = elButton(120, 276, '2F');
-  buttonTo2F.onclick = () => {
+  const elCloseButton = document.createElement('button');
+  elCloseButton.classList.add('floor-button')
+  elCloseButton.style.width = '48px';
+  elCloseButton.style.height = '48px';
+  elCloseButton.style.position = 'absolute';
+  elCloseButton.style.top = `404px`;
+  elCloseButton.style.left = `120px`;
+  elCloseButton.style.border = 'none';
+  elCloseButton.style.backgroundImage = 'url("/img/ui-map/el_close.png")';
+  elCloseButton.style.opacity = 0;
+  // elCloseButton.onmousedown(() => elCloseButton.style.opacity = 1);
+  elCloseButton.onclick = () => {
+    elCloseButton.style.opacity = 1;
     hideElevatorPanel();
-    scene.scene.start('SecondFloorScene', { x: 16 * 9, y: 16 * 21 });
+    switch (selectedFloor) {
+      case '1F':
+        scene.scene.start('FirstFloorScene', { x: 16 * 9, y: 16 * 21 });
+        break;
+      case '2F':
+        scene.scene.start('SecondFloorScene', { x: 16 * 9, y: 16 * 21 });
+        break;
+      case '5F':
+        scene.scene.start('FifthFloorScene', { x: 16 * 9, y: 16 * 22 });
+        break;
+      case '6F':
+        scene.scene.start('SixthFloorScene', { x: 16 * 9, y: 16 * 30 });
+        break;
+      case '7F':
+        scene.scene.start('SeventhFloorScene', { x: 16 * 9, y: 16 * 22 });
+        break;
+      case 'B1':
+        scene.scene.start('FirstBasementScene', { x: 16 * 9, y: 16 * 42 });
+        break;
+      case 'B2':
+        scene.scene.start('SecondBasementScene', { x: 16 * 9, y: 16 * 23 });
+        break;
+      default:
+        break;
+    }
   }
-  panel.appendChild(buttonTo2F);
 
-  const buttonTo5F = elButton(56, 212, '5F');
-  buttonTo5F.onclick = () => {
-    hideElevatorPanel();
-    scene.scene.start('FifthFloorScene', { x: 16 * 9, y: 16 * 22 });
-  }
-  panel.appendChild(buttonTo5F);
-
-  const buttonTo6F = elButton(120, 212, '6F');
-  buttonTo6F.onclick = () => {
-    hideElevatorPanel();
-    scene.scene.start('SixthFloorScene', { x: 16 * 9, y: 16 * 30 });
-  }
-  panel.appendChild(buttonTo6F);
-
-  const buttonTo7F = elButton(56, 148, '7F');
-  buttonTo7F.onclick = () => {
-    hideElevatorPanel();
-    scene.scene.start('SeventhFloorScene', { x: 16 * 9, y: 16 * 22 });
-  }
-  panel.appendChild(buttonTo7F);
-
-  const buttonToB1 = elButton(120, 340, 'B1');
-  buttonToB1.onclick = () => {
-    hideElevatorPanel();
-    scene.scene.start('FirstBasementScene', { x: 16 * 9, y: 16 * 42 });
-  }
-  panel.appendChild(buttonToB1);
-
-  const buttonToB2 = elButton(56, 340, 'B2');
-  buttonToB2.onclick = () => {
-    hideElevatorPanel();
-    scene.scene.start('SecondBasementScene', { x: 16 * 9, y: 16 * 23 });
-  }
-  panel.appendChild(buttonToB2);
+  panel.appendChild(elCloseButton);
 
   panel.style.backgroundImage = `url("/img/ui-map/el_panel_${floor}.png")`;
   document.body.appendChild(panelContainer);
