d4795f4721924ba4a1c052c6c6563ab35b621d66
leegakyeong <alaska7132@naver.com>
2021-08-27 14:00:11 +0900

    Update elevator buttons to start scene directly when pressed

--- src/entity/map/elevator.js
+++ src/entity/map/elevator.js
@@ -1,9 +1,7 @@
 let panelContainer;
-let selectedFloor;
 
-function elFloorButton(x, y, floor) {
+function elButton(x, y, floor) {
   const button = document.createElement('button');
-  button.classList.add('floor-button')
   button.style.width = '48px';
   button.style.height = '48px';
   button.style.position = 'absolute';
@@ -12,13 +10,6 @@ function elFloorButton(x, y, floor) {
   button.style.border = 'none';
   button.style.backgroundImage = `url("/img/ui-map/el_${floor}.png")`;
   button.style.opacity = 0;
-  button.onclick = () => {
-    for (const b of panelContainer.getElementsByClassName('floor-button')) {
-      b.style.opacity = 0;
-    };
-    selectedFloor = floor;
-    button.style.opacity = 1;
-  }
 
   return button;
 }
@@ -45,56 +36,74 @@ export function showElevatorPanel(scene, floor) {
 
   panelContainer.appendChild(panel);
 
-  panel.appendChild(elFloorButton(56, 276, '1F'));
-  panel.appendChild(elFloorButton(120, 276, '2F'));
-  panel.appendChild(elFloorButton(56, 212, '5F'));
-  panel.appendChild(elFloorButton(120, 212, '6F'));
-  panel.appendChild(elFloorButton(56, 148, '7F'));
-  panel.appendChild(elFloorButton(120, 340, 'B1'));
-  panel.appendChild(elFloorButton(56, 340, 'B2'));
-
-  const elCloseButton = document.createElement('button');
-  elCloseButton.classList.add('floor-button')
-  elCloseButton.style.width = '48px';
-  elCloseButton.style.height = '48px';
-  elCloseButton.style.position = 'absolute';
-  elCloseButton.style.top = `404px`;
-  elCloseButton.style.left = `120px`;
-  elCloseButton.style.border = 'none';
-  elCloseButton.style.backgroundImage = 'url("/img/ui-map/el_close.png")';
-  elCloseButton.style.opacity = 0;
-  // elCloseButton.onmousedown(() => elCloseButton.style.opacity = 1);
-  elCloseButton.onclick = () => {
-    elCloseButton.style.opacity = 1;
+  const buttonTo1F = elButton(56, 276, '1F');
+  buttonTo1F.onclick = () => {
     hideElevatorPanel();
-    switch (selectedFloor) {
-      case '1F':
-        scene.scene.start('FirstFloorScene', { x: 16 * 9, y: 16 * 21 });
-        break;
-      case '2F':
-        scene.scene.start('SecondFloorScene', { x: 16 * 9, y: 16 * 21 });
-        break;
-      case '5F':
-        scene.scene.start('FifthFloorScene', { x: 16 * 9, y: 16 * 22 });
-        break;
-      case '6F':
-        scene.scene.start('SixthFloorScene', { x: 16 * 9, y: 16 * 30 });
-        break;
-      case '7F':
-        scene.scene.start('SeventhFloorScene', { x: 16 * 9, y: 16 * 22 });
-        break;
-      case 'B1':
-        scene.scene.start('FirstBasementScene', { x: 16 * 9, y: 16 * 42 });
-        break;
-      case 'B2':
-        scene.scene.start('SecondBasementScene', { x: 16 * 9, y: 16 * 23 });
-        break;
-      default:
-        break;
-    }
+    scene.scene.start('FirstFloorScene', { x: 16 * 9, y: 16 * 21 });
   }
+  panel.appendChild(buttonTo1F);
 
-  panel.appendChild(elCloseButton);
+  const buttonTo2F = elButton(120, 276, '2F');
+  buttonTo2F.onclick = () => {
+    hideElevatorPanel();
+    scene.scene.start('SecondFloorScene', { x: 16 * 9, y: 16 * 21 });
+  }
+  panel.appendChild(buttonTo2F);
+
+  const buttonTo5F = elButton(56, 212, '5F');
+  buttonTo5F.onclick = () => {
+    hideElevatorPanel();
+    scene.scene.start('FifthFloorScene', { x: 16 * 9, y: 16 * 22 });
+  }
+  panel.appendChild(buttonTo5F);
+
+  const buttonTo6F = elButton(120, 212, '6F');
+  buttonTo6F.onclick = () => {
+    hideElevatorPanel();
+    scene.scene.start('SixthFloorScene', { x: 16 * 9, y: 16 * 30 });
+  }
+  panel.appendChild(buttonTo6F);
+
+  const buttonTo7F = elButton(56, 148, '7F');
+  buttonTo7F.onclick = () => {
+    hideElevatorPanel();
+    scene.scene.start('SeventhFloorScene', { x: 16 * 9, y: 16 * 22 });
+  }
+  panel.appendChild(buttonTo7F);
+
+  const buttonToB1 = elButton(120, 340, 'B1');
+  buttonToB1.onclick = () => {
+    hideElevatorPanel();
+    scene.scene.start('FirstBasementScene', { x: 16 * 9, y: 16 * 42 });
+  }
+  panel.appendChild(buttonToB1);
+
+  const buttonToB2 = elButton(56, 340, 'B2');
+  buttonToB2.onclick = () => {
+    hideElevatorPanel();
+    scene.scene.start('SecondBasementScene', { x: 16 * 9, y: 16 * 23 });
+  }
+  panel.appendChild(buttonToB2);
+
+  // 아래 두 개는 왜 눌러도 버튼 색이 안 변하지..? ㅜㅜ
+
+  const buttonOpen = elButton(56, 404, 'open');
+  buttonOpen.onkeydown = () => {
+    buttonOpen.style.opacity = 1;
+  }
+  buttonOpen.onkeyup = () => {
+    buttonOpen.style.opacity = 0;
+  }
+  panel.appendChild(buttonOpen);
+
+  const buttonClose = elButton(56, 404, 'close');
+  buttonClose.onkeydown = () => {
+    buttonClose.style.opacity = 1;
+  }
+  buttonClose.onkeyup = () => {
+    buttonClose.style.opacity = 0;
+  }
+  panel.appendChild(buttonClose);
 
   panel.style.backgroundImage = `url("/img/ui-map/el_panel_${floor}.png")`;
   document.body.appendChild(panelContainer);
