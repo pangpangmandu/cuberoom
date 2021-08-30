77c3899edb5923653d8f7a90d6220ff244d4cce4
leegakyeong <alaska7132@naver.com>
2021-08-30 18:16:43 +0900

    Add floor name getter function to startScene()

--- src/entity/map/startScene.js
+++ src/entity/map/startScene.js
@@ -1,5 +1,29 @@
+function getFloorName(sceneName) {
+  switch (sceneName) {
+    case 'EntranceScene':
+      return 'entrance';
+    case 'FirstFloorScene':
+      return '1F';
+    case 'SecondFloorScene':
+      return '2F';
+    case 'FifthFloorScene':
+      return '5F';
+    case 'SixthFloorScene':
+      return '6F';
+    case 'SeventhFloorScene':
+      return '7F';
+    case 'EighthFloorScene':
+      return '8F';
+    case 'FirstBasementScene':
+      return 'B1';
+    case 'SecondBasementScene':
+      return 'B2';
+  }
+}
+
 export default function startScene(scene, sceneName, spawnPos) {
   scene.cameras.main.fadeOut(500);
+  window.socket.emit('moveFloor', { id: window.socket.id, floor: getFloorName(sceneName) });
   scene.cameras.main.on('camerafadeoutcomplete', () => {
     scene.scene.start(sceneName, spawnPos);
   });
